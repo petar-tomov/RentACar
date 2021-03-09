9c57fdf275f00286b979cdb8724877fd90d48578
Petar Tomov <50597786+petar-tomov@users.noreply.github.com>
2021-03-09 23:31:03 +0200

    Created class Car

---
+++ Car.py
@@ -0,0 +1,9 @@
+class Car:
+    def __init__(self, brand, model, expense, registry_num, cost_per_hour, cost_per_day, cost_per_week):
+        self.brand = brand
+        self.model = model
+        self.expense = expense
+        self.registry_num = registry_num
+        self.cost_per_hour = cost_per_hour
+        self.cost_per_day = cost_per_day
+        self.cost_per_week = cost_per_week
