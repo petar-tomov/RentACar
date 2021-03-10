d4d6ac6ce6a195721c5a82066a034c06ad1076ed
petar-tomov <pepyy.tommyy@gmail.com>
2021-03-10 14:18:39 +0200

    Added RentACar for Hours functionality

--- System.py
+++ System.py
@@ -4,7 +4,10 @@ import json
 
 class System:
     cost = 0
-
+    toPop = False
+    removeCar = 0
+    brand = "Moskvich"  # brand and model will be used for printing information
+    model = "412"
     cars = open('data.json')
     data = json.load(cars)
     cars.close()
@@ -19,11 +22,27 @@ class System:
                 dataDict[c]['expense'], dataDict[c]['registry_num'],
                 dataDict[c]['cost_per_hour'], dataDict[c]['cost_per_day'],
                 dataDict[c]['cost_per_week'])
-        carData.update({c.brand + c.model: c})
+        carData.update({c.registry_num: c})
 
     def print_cars(self):
-        print("You can currently rent:")
+        print("Cars available for rent:")
         for c in System.carData.values():
             print(f"{c.brand} {c.model} with registry number {c.registry_num}")
             print(f"You can rent this car for ${c.cost_per_hour} per hour,"
                   f" ${c.cost_per_day} per day or ${c.cost_per_week} per week.\n")
+
+    def rentacar_hours(self, customer, registry_num, hours):
+        for c in self.carData.keys():
+            if registry_num == c:
+                self.toPop = True
+                self.cost = self.carData.get(c).cost_per_hour * hours
+                self.removeCar = c
+                customer.numRentedCars += 1
+                customer.cost += self.cost
+                self.brand = self.carData.get(c).brand
+                self.model = self.carData.get(c).model
+                print(f"{customer.name} has rented {self.brand} {self.model} "
+                      f"for {hours} hours which will cost him ${self.cost}.\n")
+
+        if self.toPop:
+            self.carData.pop(self.removeCar)
--- main.py
+++ main.py
@@ -2,4 +2,5 @@ from Customer import Customer
 
 pesho = Customer("Pesho")
 pesho.print_cars()
-
+pesho.rentacar_hours("PB5079TT", 5)
+# pesho.print_cars()  
